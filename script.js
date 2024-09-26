793afe348e43251db4f2a41bd33f465f6852c087
Anano Makharashvili <ananomakharashvili@gmail.com>
2024-09-26 19:13:37 +0300

    corrected

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const getInputRange = document.getElementById("range");
 console.log(getInputRange);
+
+getInputRange.addEventListener("input", (event) => {
+  event.target.value;
+
+  getInputRange.style.background = `linear-gradient(
+      to right,
+      hsl(174, 77%, 80%) 0%,
+      hsl(174, 77%, 80%) %,
+      hsl(224, 65%, 95%) %,
+      hsl(224, 65%, 95%) 100%
+    )`;
+});
